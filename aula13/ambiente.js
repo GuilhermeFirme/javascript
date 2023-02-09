189dfc8078149a559328439a003348ea54d0f14f
Guilherme de souza santiago <guisantiagostart@gmail.com>
2023-02-09 08:47:18 -0300

    atualizando

---
+++ aula13/ambiente.js
@@ -0,0 +1,17 @@
+console.log('Vai começar...');
+
+for (var c = 1; c <= 6; c++){
+    console.log(c)
+}
+console.log('FIM!');
+
+// var c = 1;
+// do{
+//     console.log(`Passo ${c}`);
+//     c++
+// } while(c <=  6);
+// var c = 1;
+// while(c <= 6){
+//     console.log('Tudo bem');
+//     c++;
+// }
